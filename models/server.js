1a7ddb8932c64b8f383f3d4b78ee790c58b86456
Marcos Verblud <m.verblud@naranja.com>
2022-04-04 16:28:12 -0300

    Configuracion inicial RestServer

---
+++ models/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+//const { dbConnection } = require('../database/config');
+//const fileUpload = require('express-fileupload');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.path = {
+            auth:       '/api/auth',
+            buscar:     '/api/buscar',
+            categorias: '/api/categorias',
+            productos:  '/api/productos',
+            usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads'
+        }
+
+        // Conectar a base de datos
+        this.conectarDB();
+        
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    async conectarDB(){
+    //    await dbConnection();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use(cors());
+
+        // Lectura y Parseo
+        this.app.use(express.json());
+
+        // Directorio publico
+        this.app.use(express.static('public'));
+
+        // Fileupload - Carga de Archivos
+/*         this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        })); */
+    }
+
+    routes() {
+
+    //    this.app.use(this.path.productos, require('../routes/productos'));
+
+
+    }
+
+    listen() {
+
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en el puerto ', this.port);
+        })
+    }
+
+}
+
+module.exports = Server;
